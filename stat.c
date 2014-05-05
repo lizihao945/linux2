9c5605c640e8226a835d1ff7b5a0f6c278c23759
James Lee <lizihao945@gmail.com>
2014-05-06 02:10:23 +0800

    11061089+Step6

--- stat.c
+++ stat.c
@@ -31,6 +31,11 @@ int main(int argc,char *argv[])
 	statcmd.owner=getuid();
 	statcmd.argnum=0;
 
+	#ifdef DEBUG
+		printf("statcmd type\t%d (-1 means ENQ, -2 meas DEQ, -3 means STAT)\n"
+						"statcmd owner\t%d\n",
+						statcmd.type, statcmd.owner);
+	#endif
 	if((fd=open("/tmp/server",O_WRONLY))<0)
 		error_sys("stat open fifo failed");
 
